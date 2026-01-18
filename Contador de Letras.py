de457b674231f6f3956fc1a4c480316aef5e7735
Joaquim <joaquimkoster@gmail.com>
2026-01-18 17:59:33 -0300

    Delete Contador de Letras.py

--- Contador de Letras.py
+++
@@ -1,4 +0,0 @@
-while 1:                                              # enquanto 1 for verdadeiro, e 1 sempre é verdadeiro. posso usar true também
-    palavra = input("Digite uma Palavra: ")           # define a variavel palavra
-    Quantidade = len(palavra)                         # define a variavel quantidade como palavra, len conta quantos caracteres tem dentro
-    print("A palavra tem", Quantidade, "letras")      # mostra na tela o texto, e junto o valor de quantiade
